81ab83ee42a03abebb8ebe30521a7a671ad07278
shai linde <shaylinde1@gmail.com>
2023-03-11 15:29:33 +0200

    finished lab 3

--- app/src/main/java/com/example/alllabstrue/HomeActivity.java
+++ app/src/main/java/com/example/alllabstrue/HomeActivity.java
@@ -7,15 +7,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
     private TextView msg;
+    private ListView listView;
+    private ArrayList<User> users;
+    private ArrayAdapter<User> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         msg= findViewById(R.id.textView);
+        listView= findViewById(R.id.homelistview);
+        users= new ArrayList<>();
+        users.add(new User("shai","shay", 12345));
+        users.add(new User("yaniv", "shaylindf", 345678));
+        arrayAdapter= new UserArrayAdapter(this, R.layout.user_row, users);
+        listView.setAdapter(arrayAdapter);
     }
 
     @Override
--- app/src/main/java/com/example/alllabstrue/User.java
+++ app/src/main/java/com/example/alllabstrue/User.java
@@ -1,4 +1,46 @@
 package com.example.alllabstrue;
 
 public class User {
+    private String name;
+    private String email;
+    private Integer password;
+
+    public User(String name, String email, Integer password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(Integer password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password=" + password +
+                '}';
+    }
 }
